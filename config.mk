ba7722d16ad3a5d44fd28d71b40a772db878e419
Joey Huab <joey@evolution-x.org>
2020-11-03 20:40:52 +0900

    pixelstyle: Refactor/Cleanup for 11
    * Includes new bootanimations for 5.1

--- config.mk
+++ config.mk
@@ -1,16 +1,18 @@
-# Copyright (C) 2017 The Pure Nexus Project
+#
+# Copyright (C) 2020 The Evolution X Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH := vendor/pixelstyle
 
@@ -28,33 +30,13 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
-# Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation-dark_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-     PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation-dark_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-else
-    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "PixelStyle: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
-    else
-        $(warning "PixelStyle: Current bootanimation res is not supported, forcing 1080p")
-    endif
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/bootanimation/bootanimation-dark_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation-dark.zip
-endif
-
 # Files
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/etc,$(TARGET_COPY_OUT_PRODUCT)/etc)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/fonts,$(TARGET_COPY_OUT_SYSTEM)/fonts)
 
-# Gestures
-PRODUCT_PROPERTY_OVERRIDES += \
+# Use gestures by default
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
 
 # Gboard configuration
@@ -100,31 +82,5 @@ PRODUCT_PACKAGE_OVERLAYS += \
 # Inherit from audio config
 $(call inherit-product, vendor/pixelstyle/config/audio.mk)
 
-# QS tile styles
-#PRODUCT_PACKAGES += \
-#    QStileCircleTrim \
-#    QStileDefault \
-#    QStileDualToneCircle \
-#    QStileSquircleTrim \
-#    QStileAttemptMountain \
-#    QStileDottedCircle \
-#    QStileNinja \
-#    QStilePokesign \
-#    QStileWavey \
-#    QStileCookie \
-#    QStileInkDrop \
-#    QStileSquaremedo \
-#    QStileCosmos \
-#    QStileDividedCircle \
-#    QStileNeonLight \
-#    QStileOxygen \
-#    QStileTriangles \
-#    QStileCircleOutline
-
-# QS header styles
-#PRODUCT_PACKAGES += \
-#    QSHeaderBlack \
-#    QSHeaderGrey \
-#    QSHeaderLightGrey \
-#    QSHeaderAccent \
-#    QSHeaderTransparent
+# Inherit from bootanimation config
+$(call inherit-product, vendor/pixelstyle/config/bootanimation.mk)
